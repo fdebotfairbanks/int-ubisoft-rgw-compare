6af7191dc1bde23765cd37fbc85ea791983188c8
root <f.debot@42on.com>
2026-04-14 09:58:57 +0000

    Simplified destroy_db

--- destroy_db.sh
+++ destroy_db.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
 
-PORT=5432
 
-# Batch drop
-tables=$(docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "SELECT inhrelid::regclass FROM pg_inherits WHERE inhparent = 'objects'::regclass")
-shadow2=$(docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "SELECT inhrelid::regclass FROM pg_inherits WHERE inhparent = 'multipart'::regclass")
-
-
-# Batch drop
-
-echo "$shadow2" | xargs -n 250 | while read batch; do
-    # Maak 1 DROP TABLE statement
-    sql="DROP TABLE $(echo $batch | tr ' ' ',');"
-    echo $sql
-    docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT  -U myuser -d mydb  -Atc  "$sql"
-done
-
-echo "$tables" | xargs -n 250 | while read batch; do
-    # Maak 1 DROP TABLE statement
-    sql="DROP TABLE $(echo $batch | tr ' ' ',');"
-    echo $sql
-    docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc  "$sql"
-done
+docker run --rm --net host \
+  postgres:16 \
+  psql -h 127.0.0.1 -U myuser -d postgres \
+  -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'mydb' AND pid <> pg_backend_pid();"
 
 
+docker run --rm --net host \
+  postgres:16 \
+  psql -h 127.0.0.1 -U myuser -d postgres \
+  -c "DROP DATABASE IF EXISTS mydb;"
 
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE objects CASCADE";
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE multipart CASCADE";
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE shadow CASCADE";
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE shadow2 CASCADE";
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE buckets CASCADE";
-docker run --rm --net host postgres:16 psql -h 127.0.0.1 -p $PORT -U myuser -d mydb  -Atc "DROP TABLE other CASCADE";
+docker run --rm --net host \
+  postgres:16 \
+  psql -h 127.0.0.1 -U myuser -d postgres \
+  -c "CREATE DATABASE mydb"
+
+docker run --rm --net host \
+  postgres:16 \
+  psql -h 127.0.0.1 -U myuser -d postgres \
+  -c "GRANT ALL ON DATABASE mydb TO myuser"
 
 docker exec -it redis1 redis-cli -n 2 flushdb
 docker exec -it redis1 redis-cli -n 0 flushdb
